d59cc274e56a23266d93f5524718775dabe57895
Vito <vitof970@gmail.com>
2020-04-21 11:54:13 +0200

    [Client] impemented connection to server

--- Esc2866 Client/beat_bracelet/beat_bracelet.ino
+++ Esc2866 Client/beat_bracelet/beat_bracelet.ino
@@ -3,6 +3,14 @@
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
+const char* mqttServer = "192.168.178.41";
+const int mqttPort = 1883;
+const char* mqttID = "id-user-test";
+const char* mqttUser = "test_user";
+const char* mqttPassword = "test_password";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
@@ -12,16 +20,36 @@ void setup() {
   wifiManager.autoConnect("Beat Bracelet");
 
   //TURN WIFI-LED ON
-  Serial.println("connected...yeey :)");
+  Serial.println("Wifi connected");
 
-
-  
+  //Connect to the mqtt server
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(messageReceived);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect(mqttID, mqttUser, mqttPassword )) {
+ 
+      Serial.println("Mqtt server connected");  
+ 
+    } else {
+ 
+      Serial.print("Mqtt connection failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void messageReceived(){
+  
 }
 
 void configModeCallback (WiFiManager *myWiFiManager) {
